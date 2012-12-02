07fcfef482251f2d9fb15110b64883d4949c26fe
YourGreatestMember <Ryan.Dye90@yahoo.co.uk>
2012-12-02 22:42:44 +0000

    Baal.js - Parsed through jslint courtesy of Kolton. D2BotLead.dbj - Fixed multiple channels/login commands courtesy of Kolton.

--- d2bs/kolbot/libs/bots/Baal.js
+++ d2bs/kolbot/libs/bots/Baal.js
@@ -5,94 +5,95 @@
  */
 
 function Baal() {
-
-	var RandomPrecast = Config.Baal.RandomPrecast,
+	var portal, tick,
+		RandomPrecast = Config.Baal.RandomPrecast,
 		DollQuit = Config.Baal.DollQuit,
 		SoulQuit = Config.Baal.SoulQuit,
 		KillBaal = Config.Baal.KillBaal,
 		hotTPMessage = Config.Baal.HotTPMessage,
 		safeTPMessage = Config.Baal.SafeTPMessage,
-		baalMessage = Config.Baal.BaalMessage,
-		portal, tick, baal;
+		baalMessage = Config.Baal.BaalMessage;
 
 	this.preattack = function () {
 		var check;
 		switch (me.classid) {
-			case 1:
-				// Sorceress
-				if ([49].indexOf(Config.AttackSkill[1]) > -1) {
-					if (me.getState(121)) {
-						delay(500);
-					} else {
-						Skill.cast(Config.AttackSkill[1], 0, 15094, 5028);
-					}
+		case 1:
+			// Sorceress
+			switch (Config.AttackSkill[1]) {
+			case 49:
+			case 53:
+			case 64:
+				if (me.getState(121)) {
+					delay(500);
+				} else {
+					Skill.cast(Config.AttackSkill[1], 0, 15094, 5028);
 				}
-				if ([53].indexOf(Config.AttackSkill[1]) > -1) {
-					if (me.getState(121)) {
-						delay(500);
-					} else {
-						Skill.cast(Config.AttackSkill[1], 0, 15094, 5028);
-					}
-				}
-				if ([56].indexOf(Config.AttackSkill[1]) > -1) {
-					if (me.getState(121)) {
-						delay(500);
-					} else {
-						Skill.cast(Config.AttackSkill[1], 0, 15093, 5028);
-					}
-				}
-				if ([59].indexOf(Config.AttackSkill[1]) > -1) {
-					if (me.getState(121)) {
-						delay(500);
-					} else {
-						Skill.cast(Config.AttackSkill[1], 0, 15095, 5028);
-					}
-				}
-				if ([64].indexOf(Config.AttackSkill[1]) > -1) {
-					if (me.getState(121)) {
-						delay(500);
-					} else {
-						Skill.cast(Config.AttackSkill[1], 0, 15094, 5028);
-					}
-				}
-				return true;
-			case 3:
-				// Paladin
-				if (Config.AttackSkill[3] !== 112) {
-					return false;
-				}
-				if (getDistance(me, 15094, 5029) > 3) {
-					Pather.moveTo(15094, 5029);
-				}
-				if (Config.AttackSkill[4] > 0) {
-					Skill.setSkill(Config.AttackSkill[4], 0);
-				}
-				Skill.cast(Config.AttackSkill[3], 1);
-				return true;
-			case 5:
-				// Druid
-				if (Config.AttackSkill[3] === 245) {
-					Skill.cast(Config.AttackSkill[3], 0, 15094, 5028);
-					return true;
-				}
+
 				break;
-			case 6:
-				// Assassin
-				if (Config.UseTraps) {
-					check = ClassAttack.checkTraps({
+			case 56:
+				if (me.getState(121)) {
+					delay(500);
+				} else {
+					Skill.cast(Config.AttackSkill[1], 0, 15093, 5028);
+				}
+
+				break;
+			case 59:
+				if (me.getState(121)) {
+					delay(500);
+				} else {
+					Skill.cast(Config.AttackSkill[1], 0, 15095, 5028);
+				}
+
+				break;
+			}
+
+			return true;
+		case 3:
+			// Paladin
+			if (Config.AttackSkill[3] !== 112) {
+				return false;
+			}
+
+			if (getDistance(me, 15094, 5029) > 3) {
+				Pather.moveTo(15094, 5029);
+			}
+
+			if (Config.AttackSkill[4] > 0) {
+				Skill.setSkill(Config.AttackSkill[4], 0);
+			}
+
+			Skill.cast(Config.AttackSkill[3], 1);
+
+			return true;
+		case 5:
+			// Druid
+			if (Config.AttackSkill[3] === 245) {
+				Skill.cast(Config.AttackSkill[3], 0, 15094, 5028);
+				return true;
+			}
+			break;
+		case 6:
+			// Assassin
+			if (Config.UseTraps) {
+				check = ClassAttack.checkTraps({
+					x: 15094,
+					y: 5028
+				});
+
+				if (check) {
+					ClassAttack.placeTraps({
 						x: 15094,
 						y: 5028
-					});
-					if (check) {
-						ClassAttack.placeTraps({
-							x: 15094,
-							y: 5028
-						}, 5);
-						return true;
-					}
+					}, 5);
+
+					return true;
 				}
-				break;
+			}
+
+			break;
 		}
+
 		return false;
 	};
 
@@ -103,23 +104,23 @@ function Baal() {
 			do {
 				if (Attack.checkMonster(monster) && monster.y < 5080) {
 					switch (monster.classid) {
-						case 23:
-						case 62:
-							return 1;
-						case 105:
-						case 381:
-							return 2;
-						case 557:
-							return 3;
-						case 558:
-							return 4;
-						case 571:
-							return 5;
-						default:
-							Attack.getIntoPosition(monster, 10, 0x4);
-							Attack.clear(15);
+					case 23:
+					case 62:
+						return 1;
+					case 105:
+					case 381:
+						return 2;
+					case 557:
+						return 3;
+					case 558:
+						return 4;
+					case 571:
+						return 5;
+					default:
+						Attack.getIntoPosition(monster, 10, 0x4);
+						Attack.clear(15);
 
-							return false;
+						return false;
 					}
 				}
 			} while (monster.getNext());
@@ -130,10 +131,12 @@ function Baal() {
 
 	this.clearThrone = function () {
 		var i, monster,
-		monList = [],
+			monList = [],
 			pos = [15094, 5022, 15094, 5041, 15094, 5060, 15094, 5041, 15094, 5022];
+
 		if (Config.AvoidDolls) {
 			monster = getUnit(1, 691);
+
 			if (monster) {
 				do {
 					if (monster.x >= 15072 && monster.x <= 15118 && monster.y >= 5002 && monster.y <= 5079 && Attack.checkMonster(monster) && Attack.skipCheck(monster)) {
@@ -141,10 +144,12 @@ function Baal() {
 					}
 				} while (monster.getNext());
 			}
+
 			if (monList.length) {
 				Attack.clearList(monList);
 			}
 		}
+
 		for (i = 0; i < pos.length; i += 2) {
 			Pather.moveTo(pos[i], pos[i + 1]);
 			Attack.clear(25);
@@ -163,16 +168,18 @@ function Baal() {
 							break;
 						}
 					}
+
 					break;
 				}
 			} while (monster.getNext());
 		}
+
 		return true;
 	};
 
 	this.announce = function () {
 		var count, string, souls, dolls,
-		monster = getUnit(1);
+			monster = getUnit(1);
 
 		if (monster) {
 			count = 0;
@@ -194,17 +201,14 @@ function Baal() {
 			} while (monster.getNext());
 		}
 
-		if (count > 0) {
+		if (count > 30) {
+			string = "DEADLY!!!" + " " + count + " monster" + (count > 1 ? "s " : " ") + "nearby.";
+		} else if (count > 20) {
+			string = "Lethal!" + " " + count + " monster" + (count > 1 ? "s " : " ") + "nearby.";
+		} else if (count > 10) {
+			string = "Dangerous!" + " " + count + " monster" + (count > 1 ? "s " : " ") + "nearby.";
+		} else if (count > 0) {
 			string = "Warm" + " " + count + " monster" + (count > 1 ? "s " : " ") + "nearby.";
-			if (count > 10) {
-				string = "Dangerous!" + " " + count + " monster" + (count > 1 ? "s " : " ") + "nearby.";
-				if (count > 20) {
-					string = "Lethal!" + " " + count + " monster" + (count > 1 ? "s " : " ") + "nearby.";
-					if (count > 30) {
-						string = "DEADLY!!!" + " " + count + " monster" + (count > 1 ? "s " : " ") + "nearby.";
-					}
-				}
-			}
 		} else {
 			string = "Cool TP. No immediate monsters.";
 		}
@@ -247,6 +251,7 @@ function Baal() {
 	if ((SoulQuit && getUnit(1, 691)) || (DollQuit && getUnit(1, 690))) {
 		say("Too many undead soul killers and undead stygian dolls, next game!");
 		print("Undead soul killers or Undead stygian dolls found, ending script.");
+
 		return true;
 	}
 
@@ -271,7 +276,8 @@ function Baal() {
 
 	Pather.moveTo(15094, me.classid === 3 ? 5029 : 5038);
 
-	MainLoop: while (true) {
+MainLoop:
+	while (true) {
 		if (getDistance(me, 15094, me.classid === 3 ? 5029 : 5038) > 3) {
 			Pather.moveTo(15094, me.classid === 3 ? 5029 : 5038);
 		}
@@ -281,57 +287,57 @@ function Baal() {
 		}
 
 		switch (this.checkThrone()) {
-			case 1:
-				Attack.clear(40);
+		case 1:
+			Attack.clear(40);
 
-				tick = getTickCount();
+			tick = getTickCount();
 
-				Precast.doPrecast(true);
+			Precast.doPrecast(true);
 
-				break;
-			case 2:
-				Attack.clear(40);
+			break;
+		case 2:
+			Attack.clear(40);
 
-				tick = getTickCount();
+			tick = getTickCount();
 
-				break;
-			case 4:
-				Attack.clear(40);
+			break;
+		case 4:
+			Attack.clear(40);
 
-				tick = getTickCount();
+			tick = getTickCount();
 
-				break;
-			case 3:
-				Attack.clear(40);
-				this.checkHydra();
+			break;
+		case 3:
+			Attack.clear(40);
+			this.checkHydra();
 
-				tick = getTickCount();
+			tick = getTickCount();
 
-				break;
-			case 5:
-				Attack.clear(40);
+			break;
+		case 5:
+			Attack.clear(40);
 
-				break MainLoop;
-			default:
-				if (getTickCount() - tick < 7e3) {
-					if (me.getState(2)) {
-						Skill.setSkill(109, 0);
-					}
-
-					break;
-				}
-
-				if (!this.preattack()) {
-					delay(100);
+			break MainLoop;
+		default:
+			if (getTickCount() - tick < 7e3) {
+				if (me.getState(2)) {
+					Skill.setSkill(109, 0);
 				}
 
 				break;
+			}
+
+			if (!this.preattack()) {
+				delay(100);
+			}
+
+			break;
 		}
+
 		delay(10);
 	}
 
 	if (KillBaal) {
-
 		if (Config.PublicMode) {
 			say(baalMessage);
 		}
