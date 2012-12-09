ed548366f6da77291692741b6bbba27d890816de
kolton <chvarx@gmail.com>
2012-12-10 00:59:35 +0100

    restoring lost pull from Loshmi

--- d2bs/kolbot/libs/common/Loader.js
+++ d2bs/kolbot/libs/common/Loader.js
@@ -27,8 +27,66 @@ var Loader = {
 		}
 	},
 
+	// see http://stackoverflow.com/questions/728360/copying-an-object-in-javascript#answer-728694
+	clone: function (obj) {
+		var i, copy, attr;
+
+		// Handle the 3 simple types, and null or undefined
+		if (null === obj || "object" !== typeof obj) {
+			return obj;
+		}
+
+		// Handle Date
+		if (obj instanceof Date) {
+			copy = new Date();
+
+			copy.setTime(obj.getTime());
+
+			return copy;
+		}
+
+		// Handle Array
+		if (obj instanceof Array) {
+			copy = [];
+
+			for (i = 0; i < obj.length; i += 1) {
+				copy[i] = this.clone(obj[i]);
+			}
+
+			return copy;
+		}
+
+		// Handle Object
+		if (obj instanceof Object) {
+			copy = {};
+
+			for (attr in obj) {
+				if (obj.hasOwnProperty(attr)) {
+					copy[attr] = this.clone(obj[attr]);
+				}
+			}
+
+			return copy;
+		}
+
+		throw new Error("Unable to copy obj! Its type isn't supported.");
+	},
+
+	copy: function (from, to) {
+		var i;
+
+		for (i in from) {
+			if (from.hasOwnProperty(i)) {
+				to[i] = this.clone(from[i]);
+			}
+		}
+	},
+
 	loadScripts: function () {
-		var i, townCheck;
+		var i, townCheck, reconfiguration,
+			unmodifiedConfig = {};
+
+		this.copy(Config, unmodifiedConfig);
 
 		if (!this.scriptList.length) {
 			showConsole();
@@ -40,6 +98,7 @@ ScriptLoop:
 		for (i in Scripts) {
 			if (Scripts.hasOwnProperty(i) && this.scriptList.indexOf(i) > -1 && Scripts[i]) {
 				include("bots/" + i + ".js");
+				reconfiguration = typeof Scripts[i] === 'object';
 
 				if (typeof (global[i]) === "function") {
 					if (i !== "Test" && i !== "Follower") {
@@ -55,6 +114,10 @@ ScriptLoop:
 					if (townCheck) {
 						try {
 							print("ÿc2Starting script: ÿc9" + i);
+							if (reconfiguration) {
+								print("ÿc2Copying Config properties from " + i + " object.");
+								this.copy(Scripts[i], Config);
+							}
 							global[i]();
 						} catch (e) {
 							if (this.printToOOG) {
@@ -67,6 +130,10 @@ ScriptLoop:
 								takeScreenshot();
 								delay(500);
 							}
+						}
+						if (reconfiguration) {
+							print("ÿc2Reverting back unmodified config properties.");
+							this.copy(unmodifiedConfig, Config);
 						}
 					}
 				} else {
