52eda5777200f3d713d50d203fff31630d1ed558
Samuel Schlesinger <samuelms@umich.edu>
2014-02-11 14:14:19 -0500

    Add new Beetleburst script option for Coldworm.js
    Beetleburst is a rarely run superunique in Far Oasis with a treasure
    class level of 87 and a monster level of 79. While his low monster level
    detracts from his item table, he's so quick to kill that I added him
    onto running Coldworm.
    
    Patch Details:
    - [x] Updated Coldworm.js to find and kill Beetleburst (preset 747).
    - [x] Updated Config.js and all class templates to include the new
    Config.Coldworm.KillBeetleburst option.

--- d2bs/kolbot/libs/bots/Coldworm.js
+++ d2bs/kolbot/libs/bots/Coldworm.js
@@ -1,9 +1,23 @@
+/**
+*	@filename	Coldworm.js
+*	@author		kolton, edited by 13ack.Stab
+*	@desc		kill Coldworm; optionally kill Beetleburst and clear Maggot Lair
+*/
+
 function Coldworm() {
 	var i;
 
 	Town.doChores();
 	Pather.useWaypoint(43);
 	Precast.doPrecast(true);
+	
+	// Beetleburst, added by 13ack.Stab
+	if (Config.Coldworm.KillBeetleburst) {
+		if (!Pather.moveToPreset(me.area, 1, 747))
+			throw new Error("Failed to move to Beetleburst");
+			
+		Attack.clear(15, 0, getLocaleString(2882));
+	}
 
 	for (i = 62; i <= 64; i += 1) {
 		if (!Pather.moveToExit(i, true)) {
