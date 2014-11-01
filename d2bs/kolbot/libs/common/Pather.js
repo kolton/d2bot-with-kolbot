93e1f623ebe83756f7f262b6d0626e34113779f6
cuss <dimone@rtural.ru>
2014-11-02 01:14:54 +0500

    useWaypoint: ensure a waypoint menu is opened if "check" is "true"
    When two or more bots are trying to get a waypoint simultaneously and "check" is "true", some may fail to get menu opened, hence, interact directly. D2GS derivatives handle such a behavior as a cheat and account may get a permanent ban, for an instance, Rubattle.net is the case. At Rubattle.net, "check" in the "true" state is a must. So if you wanna avoid unexpected bans at such nasty servers, just change line 796 to "if (true) {".
    
    D2GS output: 
    2014/11/1 23:40:32.140 [cheat] minx-nec(*minx-nec)@Hcs9: packet data dump: 49120000006b000000

--- d2bs/kolbot/libs/common/Pather.js
+++ d2bs/kolbot/libs/common/Pather.js
@@ -799,6 +799,23 @@ ModeLoop:
 					}
 
 					Misc.click(0, 0, wp);
+					delay(me.ping * 2);
+					tick = getTickCount();
+
+					while (!getUIFlag(0x14) && getTickCount() - tick < Math.max(Math.round((i + 1) * 1000 / (i / 5 + 1)), me.ping * 2)) {
+						print("useWaypoint: Waiting for waypoint menu");
+
+						if (getDistance(me, wp) > 5) {
+							this.moveToUnit(wp);
+						}
+
+						Misc.click(0, 0, wp);
+						delay(me.ping * 2);
+					}
+
+					if (!getUIFlag(0x14)) {
+						continue;
+					}
 
 					tick = getTickCount();
 
