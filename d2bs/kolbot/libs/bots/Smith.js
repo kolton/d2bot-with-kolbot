dc76fc2c366f24d1bfd1b7920301a8e568b837fe
unseenstorm <llodunseen@gmail.com>
2018-08-02 22:16:54 -0400

    Fixed crash when looking for smith added support to walk and clear or tele clear
    No crash and quest support

--- d2bs/kolbot/libs/bots/Smith.js
+++ d2bs/kolbot/libs/bots/Smith.js
@@ -1,20 +1,37 @@
 /**
 *	@filename	Smith.js
-*	@author		kolton
-*	@desc		kill the Smith
+*	@author		kolton//Unseen Changed to clear on the way to Smith for walking or teleporting and fixed common crash when trying to find smith and added quest check to get imbue quest if it hasn't been completed
+*	@desc		walk/teleport to kill The smith and monsters along the way and get quest 
 */
 
 function Smith() {
-	Town.doChores();
-	Pather.useWaypoint(27);
-	Precast.doPrecast(true);
-
-	if (!Pather.moveToPreset(28, 2, 108)) {
-		throw new Error("Failed to move to the Smith");
-	}
-
-	Attack.kill(getLocaleString(2889)); // The Smith
-	Pickit.pickItems();
+			 var hammer, stand, charsi;
+				Town.doChores(); 
+				Pather.useWaypoint(27, true);
+				Pather.moveToExit([27, 28], true, Config.ClearType);
+				Pather.moveToExit([28, 29], true, Config.ClearType);
+				Pather.moveToExit([29, 28], true, Config.ClearType);
+							 try {
+                    Pather.moveToPreset(28, 2, 108 , true, Config.ClearType);
+                    Attack.kill(getLocaleString(2889)); // smith
+                } catch (e) {
+                    print(e);
+					Pather.moveToPreset(me.area, 2, 108, 0, 0, Config.ClearType);
+                    Attack.clear(30); //try
+                }
+				if (!me.getQuest(3, 0)) {				
+				stand = getUnit(2, 108);
+				Misc.openChest(stand);
+				delay(500);
+				hammer = getUnit(4, 89);
+				Pickit.pickItem(hammer);
+				Town.goToTown();
+				Town.move("charsi");
+				charsi = getUnit(1, "charsi");
+				charsi.openMenu();
+				me.cancel(); ////Imbue quest
+}
+Town.goToTown();
 
 	return true;
 }
