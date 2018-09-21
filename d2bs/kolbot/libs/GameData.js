25fb2dde2e8b68cc0a0644db7309e3720bd4854e
Nishimura-Katsuo <jwebbb01@hotmail.com>
2018-09-21 10:04:16 -0600

    Fixed const naming style from THINGSANDSTUFF to THINGS_AND_STUFF

--- d2bs/kolbot/libs/GameData.js
+++ d2bs/kolbot/libs/GameData.js
@@ -4,11 +4,11 @@
 *	@desc      game data library
 */
 
-const MONSTERINDEXCOUNT = 734;
-const AREAINDEXCOUNT = 137;
+const MONSTER_INDEX_COUNT = 734;
+const AREA_INDEX_COUNT = 137;
 const SUPER = [0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 1, 0, 1, 4, 0, 2, 3, 1, 0, 1, 1, 0, 0, 0, 1, 3, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 5, 1, 1, 1, 1, 3];
-const AREALOCALESTR = [5389, 5055, 5054, 5053, 5052, 5051, 5050, 5049, 5048, 5047, 5046, 5045, 5044, 5043, 5042, 5041, 5040, 5039, 5038, 5037, 5036, 5035, 5034, 5033, 5032, 5031, 5030, 5029, 5028, 5027, 5026, 5025, 5024, 5023, 5022, 5021, 5020, 5019, 5018, 788, 852, 851, 850, 849, 848, 847, 846, 845, 844, 843, 842, 841, 840, 839, 838, 837, 836, 835, 834, 833, 832, 831, 830, 829, 828, 827, 826, 826, 826, 826, 826, 826, 826, 825, 824, 820, 819, 818, 817, 816, 815, 814, 813, 812, 810, 811, 809, 808, 806, 805, 807, 804, 845, 844, 803, 802, 801, 800, 799, 798, 797, 796, 795, 790, 792, 793, 794, 791, 789, 22646, 22647, 22648, 22649, 22650, 22651, 22652, 22653, 22654, 22655, 22656, 22657, 22658, 22659, 22660, 22662, 21865, 21866, 21867, 22663, 22664, 22665, 22667, 22666, 5389, 5389, 5389, 5018];
-const MONSTERKEYS = [
+const AREA_LOCALE_STRING = [5389, 5055, 5054, 5053, 5052, 5051, 5050, 5049, 5048, 5047, 5046, 5045, 5044, 5043, 5042, 5041, 5040, 5039, 5038, 5037, 5036, 5035, 5034, 5033, 5032, 5031, 5030, 5029, 5028, 5027, 5026, 5025, 5024, 5023, 5022, 5021, 5020, 5019, 5018, 788, 852, 851, 850, 849, 848, 847, 846, 845, 844, 843, 842, 841, 840, 839, 838, 837, 836, 835, 834, 833, 832, 831, 830, 829, 828, 827, 826, 826, 826, 826, 826, 826, 826, 825, 824, 820, 819, 818, 817, 816, 815, 814, 813, 812, 810, 811, 809, 808, 806, 805, 807, 804, 845, 844, 803, 802, 801, 800, 799, 798, 797, 796, 795, 790, 792, 793, 794, 791, 789, 22646, 22647, 22648, 22649, 22650, 22651, 22652, 22653, 22654, 22655, 22656, 22657, 22658, 22659, 22660, 22662, 21865, 21866, 21867, 22663, 22664, 22665, 22667, 22666, 5389, 5389, 5389, 5018];
+const MONSTER_KEYS = [
 	['mon1', 'mon2', 'mon3', 'mon4', 'mon5', 'mon6', 'mon7', 'mon8', 'mon9', 'mon10'],
 	['nmon1', 'nmon2', 'nmon3', 'nmon4', 'nmon5', 'nmon6', 'nmon7', 'nmon8', 'nmon9', 'nmon10'],
 ][me.diff && 1]; // mon is for normal, nmon is for nm/hell, umon is specific to picking champion/uniques in normal
@@ -45,7 +45,7 @@ const MONSTERKEYS = [
  *  .Minions = array of minions that can spawn with this unit
  */
 
-var MonsterData = Array(MONSTERINDEXCOUNT);
+var MonsterData = Array(MONSTER_INDEX_COUNT);
 
 for (let i = 0; i < MonsterData.length; i++) {
 	let index = i;
@@ -100,7 +100,7 @@ Object.freeze(MonsterData);
  *  .LocaleString = locale string index for getLocaleString
  */
 
-var AreaData = new Array(AREAINDEXCOUNT);
+var AreaData = new Array(AREA_INDEX_COUNT);
 
 for (let i = 0; i < AreaData.length; i++) {
 	let index = i;
@@ -126,8 +126,8 @@ for (let i = 0; i < AreaData.length; i++) {
 				y: getBaseStat('leveldefs', index, ['SizeY', 'SizeY(N)', 'SizeY(H)'][me.diff])
 			};
 		}, enumerable: true},
-		Monsters: {get: () => MONSTERKEYS.map(key => getBaseStat('levels', index, key)).filter(key => key !== 65535), enumerable: true},
-		LocaleString: {get: () => AREALOCALESTR[index], enumerable: true},
+		Monsters: {get: () => MONSTER_KEYS.map(key => getBaseStat('levels', index, key)).filter(key => key !== 65535), enumerable: true},
+		LocaleString: {get: () => AREA_LOCALE_STRING[index], enumerable: true},
 	}));
 }
 
