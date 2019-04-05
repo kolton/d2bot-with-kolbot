6dfa44bed696c7f571f74f39e533009ccf9e32f3
ryancrunchi <ryancrunchi@gmail.com>
2019-04-05 11:38:13 +0200

    Added new property keyword `Skin` for pickit to filter rings, amulets, jewels and charms skin. Refactored getting the skin value of item with property `skinCode` in Unit prototype.

--- d2bs/kolbot/libs/common/Prototypes.js
+++ d2bs/kolbot/libs/common/Prototypes.js
@@ -1039,6 +1039,158 @@ Unit.prototype.getColor = function () {
 	return -1;
 };
 
+Object.defineProperty(Unit.prototype, "skinCode", {
+	get: function() {
+		var code;
+		if (this.getFlag(0x10)) {
+			switch (this.quality) {
+			case 5: // Set
+				switch (this.classid) {
+				case 27: // Angelic sabre
+					code = "inv9sbu";
+
+					break;
+				case 74: // Arctic short war bow
+					code = "invswbu";
+
+					break;
+				case 308: // Berserker's helm
+					code = "invhlmu";
+
+					break;
+				case 330: // Civerb's large shield
+					code = "invlrgu";
+
+					break;
+				case 31: // Cleglaw's long sword
+				case 227: // Szabi's cryptic sword
+					code = "invlsdu";
+
+					break;
+				case 329: // Cleglaw's small shield
+					code = "invsmlu";
+
+					break;
+				case 328: // Hsaru's buckler
+					code = "invbucu";
+
+					break;
+				case 306: // Infernal cap / Sander's cap
+					code = "invcapu";
+
+					break;
+				case 30: // Isenhart's broad sword
+					code = "invbsdu";
+
+					break;
+				case 309: // Isenhart's full helm
+					code = "invfhlu";
+
+					break;
+				case 333: // Isenhart's gothic shield
+					code = "invgtsu";
+
+					break;
+				case 326: // Milabrega's ancient armor
+				case 442: // Immortal King's sacred armor
+					code = "invaaru";
+
+					break;
+				case 331: // Milabrega's kite shield
+					code = "invkitu";
+
+					break;
+				case 332: // Sigon's tower shield
+					code = "invtowu";
+
+					break;
+				case 325: // Tancred's full plate mail
+					code = "invfulu";
+
+					break;
+				case 3: // Tancred's military pick
+					code = "invmpiu";
+
+					break;
+				case 113: // Aldur's jagged star
+					code = "invmstu";
+
+					break;
+				case 234: // Bul-Kathos' colossus blade
+					code = "invgsdu";
+
+					break;
+				case 372: // Grizwold's ornate plate
+					code = "invxaru";
+
+					break;
+				case 366: // Heaven's cuirass
+				case 215: // Heaven's reinforced mace
+				case 449: // Heaven's ward
+				case 426: // Heaven's spired helm
+					code = "inv" + this.code + "s";
+
+					break;
+				case 357: // Hwanin's grand crown
+					code = "invxrnu";
+
+					break;
+				case 195: // Nalya's scissors suwayyah
+					code = "invskru";
+
+					break;
+				case 395: // Nalya's grim helm
+				case 465: // Trang-Oul's bone visage
+					code = "invbhmu";
+
+					break;
+				case 261: // Naj's elder staff
+					code = "invcstu";
+
+					break;
+				case 375: // Orphan's round shield
+					code = "invxmlu";
+
+					break;
+				case 12: // Sander's bone wand
+					code = "invbwnu";
+
+					break;
+				}
+
+				break;
+			case 7: // Unique
+				for (var i = 0; i < 401; i += 1) {
+					if (this.fname.split("\n").reverse()[0].indexOf(getLocaleString(getBaseStat(17, i, 2))) > -1) {
+						code = getBaseStat(17, i, "invfile");
+
+						break;
+					}
+				}
+
+				break;
+			}
+		}
+
+		if (!code) {
+			if (["ci2", "ci3"].indexOf(this.code) > -1) { // Tiara/Diadem
+				code = this.code;
+			} else {
+				code = getBaseStat(0, this.classid, 'normcode') || this.code;
+			}
+
+			code = code.replace(" ", "");
+
+			if ([10, 12, 58, 82, 83, 84].indexOf(this.itemType) > -1) { // ring/amu/jewel/sc/lc/gc
+				code += (this.gfx + 1);
+			}
+		}
+		
+		return code;
+	},
+	enumerable: true
+});
+
 // Object.assign polyfill from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 if (typeof Object.assign !== 'function') {
 	Object.defineProperty(Object, "assign", {
