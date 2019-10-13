71c1b252b9f6fdfb020c345f5ad37c489af3555f
Mercoory <37851307+Mercoory@users.noreply.github.com>
2019-10-14 00:20:38 +0200

    Update IPHunter.js
    @jaenster improved the variable declaration.
    
    Co-Authored-By: Jan Stoots <jaenster@users.noreply.github.com>

--- d2bs/kolbot/libs/bots/IPHunter.js
+++ d2bs/kolbot/libs/bots/IPHunter.js
@@ -6,48 +6,51 @@
 */
 
 function IPHunter() {
-	var ip = Number(me.gameserverip.split(".")[3]), i, q, w;
+    let ip = Number(me.gameserverip.split(".")[3]);
 
-	if (Config.IPHunter.IPList.indexOf(ip) > -1) {
-		D2Bot.printToConsole("IPHunter: IP found! - [" + ip + "] Game is : " + me.gamename + "//" + me.gamepassword, 7);
-		print("IP found! - [" + ip + "] Game is : " + me.gamename + "//" + me.gamepassword);
-		me.overhead(":D IP found! - [" + ip + "]");
-		me.maxgametime = 0;
+    if (Config.IPHunter.IPList.indexOf(ip) > -1) {
+        D2Bot.printToConsole("IPHunter: IP found! - [" + ip + "] Game is : " + me.gamename + "//" + me.gamepassword, 7);
+        print("IP found! - [" + ip + "] Game is : " + me.gamename + "//" + me.gamepassword);
+        me.overhead(":D IP found! - [" + ip + "]");
+        me.maxgametime = 0;
 
-		for (i = 12; i != 0; i -= 1) {
-			me.overhead(":D IP found! - [" + ip + "]" + (i-1) + " beep left");
-			beep(); // works if windows sounds are enabled
-			delay(250);
-		}
+        for (let i = 12; i > 0; i -= 1) {
+            me.overhead(":D IP found! - [" + ip + "]" + (i - 1) + " beep left");
+            beep(); // works if windows sounds are enabled
+            delay(250);
+        }
 
-		while (true) {
+        while (true) {
 
-			/* // If you want beeping at every movement
-			for (i = 12; i != 0; i -= 1) {
-				me.overhead(":D IP found! - [" + ip + "]" + (i-1) + " beep left");
-				beep(); // works if windows sounds are enabled
-				delay(250);
-			}
-			*/
+            /* // If you want beeping at every movement
+            for (let i = 12; i != 0; i -= 1) {
+                me.overhead(":D IP found! - [" + ip + "]" + (i-1) + " beep left");
+                beep(); // works if windows sounds are enabled
+                delay(250);
+            }
+            */
 
-			me.overhead(":D IP found! - [" + ip + "]");
+            me.overhead(":D IP found! - [" + ip + "]");
+            try {
+                Town.move("waypoint");
+                Town.move("stash");
+            } catch (e) {
+                // ensure it doesnt leave game by failing to walk due to desyncing.
+            }
 
-			Town.move("waypoint");
-			Town.move("stash");
+            for (let i = (12 * 60); i > 0; i -= 1) {
+                me.overhead(":D IP found! - [" + ip + "] Next movement in: " + i + " sec.");
+                delay(1000);
+            }
+        }
+    }
 
-			for (q = (12 * 60); q != 0; q -= 1) {
-				me.overhead(":D IP found! - [" + ip + "] Next movement in: " + q + " sec." );
-				delay(1000);
-			}
-		}
-	}
+    for (let i = (Config.IPHunter.GameLength * 60); i > 0; i -= 1) {
+        me.overhead(":( IP : [" + (ip) + "] NG: " + w + " sec");
+        delay(1000);
+    }
 
-	for (w = (Config.IPHunter.GameLength * 60); w != 0; w -= 1) {
-		me.overhead(":( IP : [" + (ip) + "] NG: " + w + " sec");
-		delay(1000);
-	}
-	
-	D2Bot.printToConsole("IPHunter: IP was [" + ip + "]" , 10);
+    D2Bot.printToConsole("IPHunter: IP was [" + ip + "]", 10);
 
-	return true;
+    return true;
 }
