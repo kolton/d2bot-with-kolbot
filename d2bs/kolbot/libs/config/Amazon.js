fbc351f8ec5eba9d0ebaa83a20be1e4c2529fa7f
Jesse Michelsen <jmichelsen4@gmail.com>
2020-01-08 17:29:22 -0700

    Add section to config templates for setting ClearPath

--- d2bs/kolbot/libs/config/Amazon.js
+++ d2bs/kolbot/libs/config/Amazon.js
@@ -511,6 +511,17 @@ function LoadConfig() {
 	Config.ClearType = 0xF; // Monster spectype to kill in level clear scripts (ie. Mausoleum). 0xF = skip normal, 0x7 = champions/bosses, 0 = all
 	Config.TeleStomp = false; // Use merc to attack bosses if they're immune to attacks, but not to physical damage
 
+	// Clear while traveling during bot scripts
+	// You have two methods to configure clearing. First is simply a spectype to always clear, in any area, with a default range of 30
+	// The second method allows you to specify the areas in which to clear while traveling, a range, and a spectype. If area is excluded from this method,
+	// all areas will be cleared using the specified range and spectype
+	// Config.ClearPath = 0; // Monster spectype to kill while traveling. 0xF = skip normal, 0x7 = champions/bosses, 0 = all
+	// Config.ClearPath = {
+	// 	Areas: [74],  // Specific areas to clear while traveling in. Comment out to clear in all areas
+	// 	Range: 30,  // Range to clear while traveling
+	// 	Spectype: 0,  // Monster spectype to kill while traveling. 0xF = skip normal, 0x7 = champions/bosses, 0 = all
+	// };
+
 	// Wereform setup. Make sure you read Templates/Attacks.txt for attack skill format.
 	Config.Wereform = false; // 0 / false - don't shapeshift, 1 / "Werewolf" - change to werewolf, 2 / "Werebear" - change to werebear
 
